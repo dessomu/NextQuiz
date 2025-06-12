5308d0ed645a4da5915e3115e61b971c078559b4
Somnath Baidya <somnathbaidya@Somnaths-MacBook-Air.local>
2025-06-12 18:02:58 +0530

    removed unnecessary code bocks fromlauout.js

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import  UseContextProvider  from "./context/UseContextProvider";
 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Scholars Quiz",
@@ -21,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body >
         <UseContextProvider>
         {children}
         </UseContextProvider>
